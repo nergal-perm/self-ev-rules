39c537e6cab10127dc1fd2cbafafe47547dceb7a
nergal-perm <nergal@psu.ru>
2020-06-27 12:18:34 +0500

    Added copyright and license notice as file headers
    this closes #9

--- src/main/java/ru/nergal/selferules/Term.java
+++ src/main/java/ru/nergal/selferules/Term.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright Eugene Terekhov.
+ *
+ * This file is part of Self-Evaluating-Rules (SER).
+ *
+ * SER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * or any later version as published by the Free Software Foundation.
+ *
+ * SER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SER.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package ru.nergal.selferules;
 
-/**
- * @todo #3 10m Create the short licence version to use as a source file header
- *  This short version should be referenced by Maven Qulice plugin.
- */
 public interface Term<T> {
     /**
      * @todo #3 15m Add Javadoc to Term (package-info, header, interface, method)
--- src/test/java/ru/nergal/selferules/TestPerson.java
+++ src/test/java/ru/nergal/selferules/TestPerson.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright Eugene Terekhov.
+ *
+ * This file is part of Self-Evaluating-Rules (SER).
+ *
+ * SER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * or any later version as published by the Free Software Foundation.
+ *
+ * SER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SER.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package ru.nergal.selferules;
 
 /**
