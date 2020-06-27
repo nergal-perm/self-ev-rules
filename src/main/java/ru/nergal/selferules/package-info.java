9ebb77b18026d3203f1e7498fcb6388b71ab877b
nergal-perm <nergal@psu.ru>
2020-06-27 13:28:31 +0500

    docs: Added Javadoc for Term interface, package-info for selferules package

--- src/main/java/ru/nergal/selferules/package-info.java
+++ src/main/java/ru/nergal/selferules/package-info.java
@@ -15,19 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with SER.  If not, see <https://www.gnu.org/licenses/>.
  */
-package ru.nergal.selferules;
-
 /**
- * I contain resolvable term like '2+3', so I can provide data to evaluate Rules.
- *
- * @param <T> return type of my resolution
- * @since 0.0.1
+ * Contains core Rule composition and evaluation classes.
  */
-public interface Term<T> {
-
-    /**
-     * Computes the value of the Term, delegating to all the nested Terms if needed.
-     * @return Result of the Term resolution (computation).
-     */
-    T resolve();
-}
+package ru.nergal.selferules;
